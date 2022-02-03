12e09ce3150fa9bc7e25479cf7ee77963c118b38
anandmonika <91747199+anandmonika@users.noreply.github.com>
2022-02-03 21:42:58 +0530

    Add files via upload

---
+++ 02.FUNCTIONS/demo/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+
+    public static int sum(int x,int y){
+        int ans=x+y;
+        return ans;
+    }
+
+    public static void main(String[] args){
+        int x=10;
+        int y=20;
+        int answer=sum(x,y);
+        System.out.println(answer);
+    }
+}
